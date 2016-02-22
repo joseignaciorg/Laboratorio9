310b4e341433a50e86f81201d692c9801c220236
Jose Ignacio Rodríguez García <JoseIgnacio.Rodriguez@tajamar365.com>
2016-02-22 14:09:50 +0100

    no message

---
+++ Laboratorio9/Controllers/FotoController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Laboratorio9.Utils;
+
+namespace Laboratorio9.Controllers
+{
+    public class FotoController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            var almacen = ConfigurationManager.AppSettings["container"];
+            var cuenta = ConfigurationManager.AppSettings["cuenta"];
+            var clave = ConfigurationManager.AppSettings["clave"];
+
+            var st=new Storage(cuenta, clave);
+
+            var l = st.ListaContenedor(almacen);
+
+
+            return View(l);
+        }
+
+        public ActionResult Nueva()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nueva(HttpPostedFileBase fichero)
+        {
+            var cuenta = ConfigurationManager.AppSettings["cuenta"];
+            var clave = ConfigurationManager.AppSettings["clave"];
+
+            var st=new Storage(cuenta,clave);
+            var almacen = ConfigurationManager.AppSettings["container"];
+
+            if (fichero!=null && fichero.ContentLength>0)
+            {
+                var nombre = DateTime.Now.Ticks.ToString();
+                st.SubirFoto(fichero.InputStream, nombre,almacen);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Borrar(String id)
+        {
+            var cuenta = ConfigurationManager.AppSettings["cuenta"];
+            var clave = ConfigurationManager.AppSettings["clave"];
+
+            var st = new Storage(cuenta, clave);
+            var almacen = ConfigurationManager.AppSettings["container"];
+
+            st.BorrarFoto(id,almacen);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Laboratorio9/Utils/Storage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Laboratorio9.Utils
+{
+    public class Storage
+    {
+        private CloudStorageAccount cuentaAlmacenamiento;
+
+        public Storage(String cuenta, String clave)
+        {
+            StorageCredentials cred=new StorageCredentials(cuenta,clave);
+            cuentaAlmacenamiento= new CloudStorageAccount(cred,true);
+        }
+
+        private void ComprobarContainer(String contenedor)
+        {
+            CloudBlobClient bobClient = cuentaAlmacenamiento.CreateCloudBlobClient();
+            CloudBlobContainer container = bobClient.GetContainerReference(contenedor);
+            
+            container.CreateIfNotExists();  
+        }
+
+        public List<CloudBlockBlob> ListaContenedor(String contenedor)
+        {
+            ComprobarContainer(contenedor);
+            CloudBlobClient blobClient = cuentaAlmacenamiento.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(contenedor);
+
+            List<CloudBlockBlob>urls=new List<CloudBlockBlob>();
+
+            foreach (IListBlobItem item in container.ListBlobs(null,false))
+            {
+                if (item is CloudBlockBlob)
+                {
+                    var blob = item as CloudBlockBlob;
+                    urls.Add(blob);
+                }
+            }
+
+            return urls;
+        }
+
+        public void SubirFoto(Stream foto, String nombre, String contenedor)
+        {
+            ComprobarContainer(contenedor);
+            CloudBlobClient blobClient = cuentaAlmacenamiento.CreateCloudBlobClient();
+            CloudBlobContainer container=blobClient.GetContainerReference(contenedor);
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(nombre);
+            blockBlob.UploadFromStream(foto);
+            foto.Close();
+        }
+
+        public void BorrarFoto(String foto, String contenedor)
+        {
+           ComprobarContainer(contenedor);
+            CloudBlobClient blobClient = cuentaAlmacenamiento.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(contenedor);
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(foto);
+            blockBlob.Delete();
+
+        }
+    }
+}
